aeb3048618a281547e9fe72d038eea7060c9d58d
Jeydin31 <81248955+Jeydin31@users.noreply.github.com>
2021-03-23 13:43:06 -0500

    Update start-giveaway.js

--- commands/start-giveaway.js
+++ commands/start-giveaway.js
@@ -3,36 +3,36 @@ const ms = require('ms');
 exports.run = async (client, message, args) => {
 
     // If the member doesn't have enough permissions
-    if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
-        return message.channel.send(':x: You need to have the manage messages permissions to start giveaways.');
+    if(!message.member.hasPermission('MANAGE_GUILD') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
+        return message.channel.send(':x: | You need to have the Manage Server permission to start giveaways!');
     }
 
     // Giveaway channel
     let giveawayChannel = message.mentions.channels.first();
     // If no channel is mentionned
     if(!giveawayChannel){
-        return message.channel.send(':x: You have to mention a valid channel!');
+        return message.channel.send(':x: | You have to mention a valid channel!');
     }
 
     // Giveaway duration
     let giveawayDuration = args[1];
     // If the duration isn't valid
     if(!giveawayDuration || isNaN(ms(giveawayDuration))){
-        return message.channel.send(':x: You have to specify a valid duration!');
+        return message.channel.send(':x: | You have to specify a valid duration!');
     }
 
     // Number of winners
     let giveawayNumberWinners = args[2];
     // If the specified number of winners is not a number
     if(isNaN(giveawayNumberWinners) || (parseInt(giveawayNumberWinners) <= 0)){
-        return message.channel.send(':x: You have to specify a valid number of winners!');
+        return message.channel.send(':x: | You have to specify a valid number of winners!');
     }
 
     // Giveaway prize
     let giveawayPrize = args.slice(3).join(' ');
     // If no prize is specified
     if(!giveawayPrize){
-        return message.channel.send(':x: You have to specify a valid prize!');
+        return message.channel.send(':x: | You have to specify a valid prize!');
     }
 
     // Start the giveaway
